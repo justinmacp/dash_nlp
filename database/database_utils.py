b7b8762d3f7886d552063cb2d4d06ca7b1c53415
MacPherson Justin <justin.macpherson@scs.ch>
2020-12-27 16:29:24 +0100

    added execute query and execute read query functions

--- database/database_utils.py
+++ database/database_utils.py
@@ -2,13 +2,76 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_connection(host_name, user_name, user_password):
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT, 
+  name TEXT NOT NULL, 
+  age INT, 
+  gender TEXT, 
+  nationality TEXT, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id INT AUTO_INCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+create_users = """
+INSERT INTO
+  `users` (`name`, `age`, `gender`, `nationality`)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+
+update_post_description = """
+UPDATE
+  posts
+SET
+  description = "The weather has become pleasant now"
+WHERE
+  id = 2
+"""
+
+delete_comment = "DELETE FROM comments WHERE id = 2"
+
+select_users = "SELECT * FROM users"
+
+
+def create_connection_to_mysql(host_name, user_name, user_password):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
             passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+def create_connection_to_db(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -26,5 +89,38 @@ def create_database(connection, query):
         print(f"The error '{e}' occurred")
 
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
 if __name__ == '__main__':
-    connection = create_connection("localhost", "root", "")
+    local_connection = create_connection_to_mysql("localhost", "root", "Ju5Ky1M@")
+    create_database_query = "CREATE DATABASE sm_app"
+    create_database(local_connection, create_database_query)
+    new_connection = create_connection_to_db("localhost", "root", "Ju5Ky1M@", "sm_app")
+    execute_query(new_connection, create_users_table)
+    execute_query(new_connection, create_posts_table)
+    execute_query(new_connection, create_users)
+    users = execute_read_query(new_connection, select_users)
+    for user in users:
+        print(user)
+    execute_query(new_connection, update_post_description)
+    execute_query(new_connection, delete_comment)
+    for user in users:
+        print(user)
